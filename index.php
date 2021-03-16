c40975ba20d17ed66fdd5c560bf87d7ac62681f6
wspras <wspras@gmail.com>
2021-03-16 22:22:23 +0700

    penambahan file index

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Used to setup and fix common variables and include
+ * the WSPRAS SITE procedural and class library.
+ *
+ * You should not have to change this file and allows
+ * for some configuration all web content
+ *
+ * @package WSPRAS
+ */
+define ("WEB_TITLE","WSPRAS Directory");
+define ("WEB_NAME","WSPRAS Directory Package");
+  $system_path = 'static/php/dir_web.php';
+  if ((include $system_path) != TRUE) {
+    include "/" . $system_path;
+  }
+exit();
