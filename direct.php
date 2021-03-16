bf452228a94dcc3d363da9f4f0108bbea604636b
wspras <wspras@gmail.com>
2021-03-16 22:31:07 +0700

    Penambahan file direct

---
+++ direct.php
@@ -0,0 +1,41 @@
+<?php
+# $Id: freeware.php,v 1.12.2.3 2005/01/15 11:48:23 wspras Exp $
+//----------------------------------------------------------------------
+// wistekom.com
+//----------------------------------------------------------------------
+// Copyright (c) 2005 WSPRAS
+//----------------------------------------------------------------------
+// This program is under the terms of the GENERAL PUBLIC LICENSE (GPL)
+// as published by the SOFTWARE FOUNDATION. The GPL is available
+// through the world-wide-web at http://www.wspras.com
+//----------------------------------------------------------------------
+// Authors: wspras@wistekom.com
+/** Define ABSPATH as this files directory */
+if ( !defined('_IN_SITE_WEB') )
+	define('_IN_SITE_WEB', TRUE);
+	
+define( 'ABSPATH', dirname(__FILE__) . '/' );
+
+if ( defined('E_RECOVERABLE_ERROR') )
+	error_reporting(E_ERROR | E_WARNING | E_PARSE | E_USER_ERROR | E_USER_WARNING | E_RECOVERABLE_ERROR);
+else
+	error_reporting(E_ERROR | E_WARNING | E_PARSE | E_USER_ERROR | E_USER_WARNING);
+
+if ( file_exists( ABSPATH . '/bases/config.app.php') ) {
+
+	/** The config file resides in ABSPATH */
+	require_once( ABSPATH . '/bases/config.app.php' );
+
+} elseif ( file_exists( dirname(ABSPATH) . '/bases/config.app.php' ) && ! file_exists( dirname(ABSPATH) . '/bases/config.app.php' ) ) {
+
+	/** The config file resides one level above ABSPATH but is not part of another install*/
+	require_once( dirname(ABSPATH) . '/bases/config.app.php' );
+
+} else {
+
+	// A config file doesn't exist
+	// Die with an error message
+	die("There doesn't seem to be a <code>config.app.php</code> file. I need this before we can get started. Need more help? <a href='http://wspras.com/Editing_config-app.php'>We got it</a>. You can create a <code>config.app.php</code> file through a web interface, but this doesn't work for all server setups. The safest way is to manually create the file.</p>");
+
+}
+?>
